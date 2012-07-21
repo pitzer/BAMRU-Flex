aa0eb1571e90ac9f09012dd7ea41f645bb0becc8
andyl <andy@r210.com>
2012-07-21 12:28:08 -0700

    updated production capfile

--- config/deploy/devstage.rb
+++ config/deploy/devstage.rb
@@ -10,6 +10,7 @@ server proxy, :app, :web, :db, :primary => true
 # setup vhost names in /etc/hosts
 after  "deploy", "ghost:remote"
 after  "deploy", "ghost:local"
+
 before "deploy:restart", :precompile_assets
 
 task :precompile_assets do
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,3 +6,9 @@ set :branch,    fetch(:branch, "master")
 set :rails_env, fetch(:env,    "production")
 
 server proxy, :app, :web, :db, :primary => true
+
+before "deploy:restart", :precompile_assets
+
+task :precompile_assets do
+  run "cd #{release_path}; rake assets:precompile"
+end
